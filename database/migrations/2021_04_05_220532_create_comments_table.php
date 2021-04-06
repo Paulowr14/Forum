66286d7a74fe6dd7cff9b99af3371fd520d28194
Paulo Victor <pvictor.ti@gmail.com>
2021-04-06 19:03:40 -0300

    inclusão de migrates

--- database/migrations/2021_04_05_220532_create_comments_table.php
+++ database/migrations/2021_04_05_220532_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
             $table->timestamps();
         });
     }
